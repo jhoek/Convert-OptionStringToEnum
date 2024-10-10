5cf399de1499dbd01d604e8a2cc30682e96bf9e5
Jan Hoek <jhoek@uncommonsense.nl>
2024-10-10 11:13:08 +0200

    Preparations

--- Convert-OptionStringToEnum.Tests.ps1
+++ Convert-OptionStringToEnum.Tests.ps1
@@ -7,11 +7,11 @@ Describe 'Convert-OptionStringToEnum' {
         Convert-OptionStringToEnum -OptionString 'Foo,,Baz' | Should -Be @('    value(0; Foo)', '    {', '        Caption=''Foo'';', '    }', '    value(2; Baz)', '    {', '        Caption=''Baz'';', '    }')
     }
 
-    # It 'Converts option strings that contain values with spaces' {
+    It 'Converts option strings that contain values with spaces' {
+        Convert-OptionStringToEnum -OptionString '"Foo Baz",Bar' | Should -Be @('    value(0; "Foo Baz")', '    {', '        Caption=''Foo Baz'';', '    }', '    value(1; Bar)', '    {', '        Caption=''Bar'';', '    }')
+    }
 
-    # }
-
-    # It 'Converts option captions' {
-
-    # }
+    It 'Converts option captions' {
+        Convert-OptionStringToEnum -OptionString 'Foo,Baz' -DevComment 'Bar,Qux' | Should -Be @('    value(0; Foo)', '    {', '        Caption=''Foo''; Comment=''nl-NL=Bar''', '    }', '    value(1; Baz)', '    {', '        Caption=''Baz''; Comment=''nl-NL=Qux''', '    }')
+    }
 }
